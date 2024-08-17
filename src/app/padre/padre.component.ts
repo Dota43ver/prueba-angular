33460e30bea0c6fc8ae219bd724d89a92671d7e1
Dota43ver <facundoagueroc@gmail.com>
2024-08-17 12:47:24 -0300

    mensaje enviado desde componente padre a componente hijo

--- src/app/padre/padre.component.ts
+++ src/app/padre/padre.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class PadreComponent {
 
   valorContador: number = 0;
+  mensajePadre = 'mensaje desde padre';
 
   incrementar(){
     this.valorContador++
